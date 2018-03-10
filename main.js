b9c39d2e2c768f5ea460394da2a274aad4ed9131
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-03-10 13:49:41 -0600

    added spanish and german data

--- main.js
+++ main.js
@@ -1,1 +1,66 @@
 console.log('Stix');
+
+// Spanish variable (Array of Objects)
+  var spanish = [
+      {
+        "merry": "alegre",
+        "christmas":  "navidad",
+        "happy": "feliz",
+        "new": "nuevo",
+        "year": "ano",
+        "have": "tener",
+        "a": "un",
+        "and": "y",
+        "xmas": "xmas",
+        "holiday": "fiesta",
+        "we": "nosotros",
+        "wish": "deseo",
+          "you": "tú"
+      },
+
+  ];
+
+  // German variable  (Array of Objects)
+  var german = [
+      {
+          "merry": "fröhlich",
+        "christmas":  "navidad",
+          "happy": "weihnachten",
+        "new": "neu",
+        "year": "jahr",
+        "have": "haben",
+        "a": "ein",
+        "and": "und",
+        "xmas": "xmas",
+        "holiday": "urlaub",
+        "we": "wir",
+        "wish": "wunsch",
+        "you": "sie"
+      },
+
+  ];
+
+  // Italian variable (Array of Objects)
+  var italian = [
+      {
+        "merry": "feliz",
+        "christmas":  "navidad",
+        "happy": "",
+        "new": "",
+        "year": "",
+        "have": "",
+        "a": "",
+        "and": "",
+        "xmas": "",
+        "holiday": "",
+        "we": "",
+        "wish": "",
+        "you": ""
+      },
+
+  ];
+
+
+
+// Fill all objects with limted dictionary.
+// Limited Dictionary should include: Merry, Christmas, Happy, New, Year, Have, A, And, Xmas, Holiday, We, Wish, You
