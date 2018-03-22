63a310a14f7873984649cef068f86a3e55c318a8
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-03-22 11:15:14 -0500

    code working for one letter not a phrase with help from class assignment

--- main.js
+++ main.js
@@ -16,53 +16,59 @@ const spanish = [
       }
     ];
 
-  // const german = [
-  //     {
-  //       merry: "fröhlich",
-  //       christmas:  "navidad",
-  //       happy: "weihnachten",
-  //       new: "neu",
-  //       year: "jahr",
-  //       have: "haben",
-  //       a: "ein",
-  //       and: "und",
-  //       xmas: "xmas",
-  //       holiday: "urlaub",
-  //       we: "wir",
-  //       wish: "wunsch",
-  //       you: "sie"
-  //     }
-  //   ];  
+  const german = [
+      {
+        merry: "fröhlich",
+        christmas:  "navidad",
+        happy: "weihnachten",
+        new: "neu",
+        year: "jahr",
+        have: "haben",
+        a: "ein",
+        and: "und",
+        xmas: "xmas",
+        holiday: "urlaub",
+        we: "wir",
+        wish: "wunsch",
+        you: "sie"
+      }
+    ];  
 
-  // const italian = [
-  //     {
-  //       merry: "buon",
-  //       christmas:  "natale",
-  //       happy: "contento",
-  //       new: "nuovo",
-  //       year: "anno",
-  //       have: "avere",
-  //       a: "un",
-  //       and: "e",
-  //       xmas: "xmas",
-  //       holiday: "vacanza",
-  //       we: "noi",
-  //       wish: "desiderio",
-  //       you: "tu"
-  //     }
-  //   ];  
-console.log("type of", typeof spanish);
+  const italian = [
+      {
+        merry: "buon",
+        christmas:  "natale",
+        happy: "contento",
+        new: "nuovo",
+        year: "anno",
+        have: "avere",
+        a: "un",
+        and: "e",
+        xmas: "xmas",
+        holiday: "vacanza",
+        we: "noi",
+        wish: "desiderio",
+        you: "tu"
+      }
+    ];  
 
 const outputDiv = document.getElementById('output-container');
 const getText = document.getElementById('text-box');
 
 const allTheButtons = document.getElementsByClassName('translate');
 for (let i=0; i< allTheButtons.length; i++){
-  allTheButtons[i].addEventListener('click', (e) =>{
+  allTheButtons[i].addEventListener('click', (event) =>{
     const userInput = getText.value;
       let domOutput = '';
-      if (e.target.id === 'spanish-btn'){
-        domOutput = spanish[userInput];
+      const spanishObj = spanish[0];
+      const germanObj = german[0];
+      const italianObj = italian[0];
+      if (event.target.id === 'spanish-btn'){
+        domOutput = spanishObj[userInput];
+      } else if (event.target.id === 'german-btn'){
+        domOutput = germanObj[userInput];
+      } else {
+        domOutput = italianObj[userInput];
       }
       outputDiv.innerHTML = domOutput;
   });
